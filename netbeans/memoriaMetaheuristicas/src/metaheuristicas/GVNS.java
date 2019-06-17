89cbd6990793991e6c550627c40150a4096ae4a3
mayconamaro <maycon.amaro97@gmail.com>
2019-06-17 08:00:29 -0300

    paula

---
+++ netbeans/memoriaMetaheuristicas/src/metaheuristicas/GVNS.java
@@ -0,0 +1,19 @@
+package metaheuristicas;
+
+/**
+ * Implementação do Variable Neighbourhood Search
+ * usando como buscal local o Variable Neighbourhood Descent.
+ * Também conhecido como General VNS.
+ * 
+ * @author Maycon Amaro e Paula Toledo
+ */
+
+public class GVNS {
+    
+    public void rodar(){
+        
+        
+        
+    }
+    
+}
